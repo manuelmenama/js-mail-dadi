336a151ec963040c2c4ce34f61993d1f8308e579
manuelmenama <manuel.mena090589@gmail.com>
2022-10-14 17:20:53 +0200

    completo

--- verifica-email/js/script.js
+++ verifica-email/js/script.js
@@ -1,22 +1,28 @@
 //prendo il valore dell'email dall'utente
 const writedEmail = document.getElementById('email');
 const verfyButton = document.getElementById('button');
+const output = document.querySelector('h2');
 
 //creo un'array di email accettate
 const nukeEmail = ["xi-jinping@nuke.com", "vladimir-putin@nuke.com", "joe-biden@nuce.com", "kim-jong-un@nuke.com", "emmanuel-macron@nuke.com" , "narendra-modi@nuke.com", "shehbaz-sharif@nuke.com"];
+let isNukePresident = false;
 
 console.log(writedEmail.value);
 //quando clicca sul bottone
 verfyButton.addEventListener('click', function(){
   console.log(writedEmail.value);
+  //scorro nell'array per verificare che sia presente l'email inserita
   for (let i = 0; i < nukeEmail.length; i++) {
     if(writedEmail.value === nukeEmail[i]){
-      console.log('You are a nuke president!');
+      isNukePresident = true;
+      console.log('vero');
     }
-    
+  }
+  //se presente messaggio positivo
+  //se non presente messaggio negativo
+  if(isNukePresident){
+    output.innerHTML = `Hi Mr President, you are @nuke.com president!`;
+  }else{
+    output.innerHTML = `Sorry but you aren't @nuke.com president`;
   }
 })
-  //scorro nell'array per verificare che sia presente l'email inserita
-    //se presente messaggio positivo
-    //se non presente messaggio negativo
-
