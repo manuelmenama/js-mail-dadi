6775837204140623bbb6a0da944f8b27e20ffb68
manuelmenama <manuel.mena090589@gmail.com>
2022-10-14 18:07:06 +0200

    complete but only console

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,23 @@
-console.log('ciao dadi');
+const playYourGameButton = document.getElementById('button');
+
+let dadoHuman;
+let dadoBrowser;
+
+
+playYourGameButton.addEventListener('click', function(){
+  dadoHuman = Math.ceil(Math.random() * 6);
+  dadoBrowser = Math.ceil(Math.random() * 6);
+  
+  
+  if(dadoHuman < dadoBrowser){
+    console.log(dadoHuman, dadoBrowser, 'browser');
+  }else if(dadoHuman > dadoBrowser){
+    console.log(dadoHuman, dadoBrowser, 'human');
+  }else{
+    console.log(dadoHuman, dadoBrowser, 'even');
+  }
+
+  //solo logica
+     
+  
+});
--- verifica-email/js/script.js
+++ verifica-email/js/script.js
@@ -4,25 +4,30 @@ const verfyButton = document.getElementById('button');
 const output = document.querySelector('h2');
 
 //creo un'array di email accettate
-const nukeEmail = ["xi-jinping@nuke.com", "vladimir-putin@nuke.com", "joe-biden@nuce.com", "kim-jong-un@nuke.com", "emmanuel-macron@nuke.com" , "narendra-modi@nuke.com", "shehbaz-sharif@nuke.com"];
+const nukeEmail = ["xi-jinping@nuke.com", "vladimir-putin@nuke.com", "joe-biden@nuke.com", "kim-jong-un@nuke.com", "emmanuel-macron@nuke.com" , "narendra-modi@nuke.com", "shehbaz-sharif@nuke.com"];
 let isNukePresident = false;
+
 
 console.log(writedEmail.value);
 //quando clicca sul bottone
-verfyButton.addEventListener('click', function(){
-  console.log(writedEmail.value);
-  //scorro nell'array per verificare che sia presente l'email inserita
-  for (let i = 0; i < nukeEmail.length; i++) {
-    if(writedEmail.value === nukeEmail[i]){
-      isNukePresident = true;
-      console.log('vero');
+
+  verfyButton.addEventListener('click', function(){
+    console.log(writedEmail.value);
+    //scorro nell'array per verificare che sia presente l'email inserita
+    for (let i = 0; i < nukeEmail.length; i++) {
+      if(writedEmail.value === nukeEmail[i]){
+        isNukePresident = true;
+        console.log('vero');
+      }
     }
-  }
-  //se presente messaggio positivo
-  //se non presente messaggio negativo
-  if(isNukePresident){
-    output.innerHTML = `Hi Mr President, you are @nuke.com president!`;
-  }else{
-    output.innerHTML = `Sorry but you aren't @nuke.com president`;
-  }
-})
+    //se presente messaggio positivo
+    //se non presente messaggio negativo
+    if(isNukePresident){
+      output.innerHTML = `Hi Mr President, you are @nuke.com president!`;
+      writedEmail.value = "";
+    }else{
+      output.innerHTML = `Sorry but you aren't @nuke.com president...`;
+      writedEmail.value = "";
+    }
+  });
+
