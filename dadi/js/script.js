60f14575d9ceee7e76420d5fc5c6ea7bf2014eb2
manuelmenama <manuel.mena090589@gmail.com>
2022-10-15 10:48:00 +0200

    complete

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,19 +1,38 @@
 const playYourGameButton = document.getElementById('button');
+const humanResult = document.getElementById('human');
+const browserResult = document.getElementById('browser');
+let whoWin = document.getElementById('who-win');
 
 let dadoHuman;
 let dadoBrowser;
+
+humanResult.innerHTML = "";
+browserResult.innerHTML = "";
+whoWin.innerHTML = "";
+
 
 
 playYourGameButton.addEventListener('click', function(){
   dadoHuman = Math.ceil(Math.random() * 6);
   dadoBrowser = Math.ceil(Math.random() * 6);
   
+  humanResult.innerHTML = `Human plays ${dadoHuman}`;
+  browserResult.innerHTML = `Browser plays ${dadoBrowser}`;
   
   if(dadoHuman < dadoBrowser){
+    whoWin.className = "";
+    whoWin.innerHTML = `You loose...`;
+    whoWin.classList.add('red');
     console.log(dadoHuman, dadoBrowser, 'browser');
   }else if(dadoHuman > dadoBrowser){
+    whoWin.className = "";
+    whoWin.innerHTML = `You win!`;
+    whoWin.classList.add('green');
     console.log(dadoHuman, dadoBrowser, 'human');
   }else{
+    whoWin.className = "";
+    whoWin.innerHTML = `Even.`;
+    whoWin.classList.add('blue');
     console.log(dadoHuman, dadoBrowser, 'even');
   }
 
